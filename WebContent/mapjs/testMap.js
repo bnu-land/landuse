e99731435bf1ff901bf40d1a9d07343e1975c61d
wucan <wucangeo@gmail.com>
2015-05-20 16:03:14 +0800

    首次提交项目，包含全部界面。

---
+++ WebContent/mapjs/testMap.js
@@ -0,0 +1,11 @@
+/**
+ * Created by WUCAN on 2015/5/18.
+ */
+
+require(["esri/map", "dojo/domReady!"], function(Map) {
+    var map = new Map("map", {
+        center: [-118, 34.5],
+        zoom: 8,
+        basemap: "topo"
+    });
+});
